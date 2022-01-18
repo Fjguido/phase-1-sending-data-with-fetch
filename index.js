4e02c82d705c15a61e398e0668bc38350ea2d0db
Francis Guido <fjguido97@gmail.com>
2022-01-18 01:07:16 -0500

    commit tree eb3e56ac9b909653f5392b7567dc273b92dd2330

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Add your code here
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify({
+//         dogName: "Byron",
+//         dogBreed: "Poodle",
+//     }), 
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (res) {
+//         return res.json();
+//     })
+//     .then(function(object){
+//         console.log(object);
+//     })
+//     .catch(function(error){
+//         alert("Bad thigns! Ragnork!");
+//         console.log(error.message);
+//     })
+function submitData (name, email){
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+
+        },
+        body: JSON.stringify({
+            name: "Steve",
+            email: "steve@steve.com",
+        }),
+    })
+    .then(function (res){
+        return res.json();
+    })
+    .then(function(object){
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
+}
+
+
+
+
